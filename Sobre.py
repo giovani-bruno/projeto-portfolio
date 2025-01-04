d68338be8229297c9315cac9f091c60d590ff12d
Giovani Bruno <giovainic@gmail.com>
2025-01-04 19:26:00 -0300

    commit 3

--- Sobre.py
+++ Sobre.py
@@ -3,37 +3,52 @@ import base64
 
 st.set_page_config(page_title="Portfólio", layout='wide')
 st.title("Olá, me chamo Giovani 👋🏻")
-# st.subheader("Estudante de Ciência da Computação | Entusiasta na área de Ciência de Dados")
-st.image("imagens/perfil_linkedin.png")
-st.write("""
-Sou estudante de Ciência da Computação e apaixonado por transformar dados em soluções práticas.  
-Atualmente, desenvolvo projetos voltados para análise e visualização de dados, automação de processos e otimização utilizando Python e ferramentas como pandas, Streamlit, Power BI e SQL.  
-Meu objetivo é construir uma carreira sólida em ciência de dados, combinando habilidades técnicas com uma visão estratégica. Sempre busco aprender e crescer, enfrentando desafios com entusiasmo e criatividade.  
-""")
-
-st.subheader("Habilidades")
-
-
-
-col1, col2 = st.columns([1, 1])
-
+col1, col2 = st.columns([0.1, 1])
 with col1:
-    st.markdown(
-        """<a href="https://www.instagram.com/giovanibrn_/">
-        <img src="data:image/png;base64,{}" width="50">
-        </a>""".format(
-            base64.b64encode(open("imagens/logo_instagram.png", "rb").read()).decode()
-        ),
-        unsafe_allow_html=True,
-    )
-
-
-with col2:
-    st.markdown(
+    st.image("imagens/giovani.jpeg", width=150)
+    c1, c2, c3 = st.columns(3)
+    c1.markdown(
         """<a href="https://www.linkedin.com/in/giovani-bruno-dos-santos-costa/">
         <img src="data:image/png;base64,{}" width="50">
         </a>""".format(
             base64.b64encode(open("imagens/logo_linkedin.png", "rb").read()).decode()
         ),
-        unsafe_allow_html=True,
+        unsafe_allow_html=True
     )
+    c2.markdown(
+        """<a href="https://github.com/giovani-bruno">
+        <img src="data:image/png;base64,{}" width="50">
+        </a>""".format(
+            base64.b64encode(open("imagens/logo_github.png", "rb").read()).decode()
+        ),
+        unsafe_allow_html=True
+
+    )
+    c3.markdown(
+        """<a href="https://www.instagram.com/giovanibrn_/">
+        <img src="data:image/png;base64,{}" width="50">
+        </a>""".format(
+            base64.b64encode(open("imagens/logo_instagram.png", "rb").read()).decode()
+        ),
+        unsafe_allow_html=True
+    )
+with col2:
+    st.write("""
+    Sou estudante de Ciência da Computação e apaixonado por transformar dados em soluções práticas.
+             """)
+st.divider()
+
+st.subheader("⚒️ Habilidades")
+col1, col2, col3, col4 = st.columns(4)
+col1.image("imagens/logo_python.png", width=150)
+col2.image("imagens/logo_powerbi.png", width=50)
+col3.image("imagens/logo_excel.png", width=50)
+col4.image("imagens/logo_git.png", width=50)
+col1.image("imagens/logo_sql.png", width=50)
+col2.image("imagens/logo_pandas.png", width=150)
+col3.image("imagens/logo_numpy.png", width=150)
+col4.image("imagens/logo_matplotlib.png", width=150)
+col1.image("imagens/logo_seaborn.png", width=150)
+col2.image("imagens/logo_plotly.png", width=100)
+col3.image("imagens/logo_streamlit.png", width=150)
+col4.image("imagens/logo_sklearn.png", width=100)
--- pages/Certificados.py
+++ pages/Certificados.py
@@ -14,35 +14,42 @@ def adicionar_certificado(certificado, duracao, data_inicio, data_conclusao, res
     col2.write(f"Iniciado em: {data_inicio}")
     col3.write(f"Concluído em: {data_conclusao}")
     st.write(resumo)
-    pdf_viewer(certificado, width=725, height=500, resolution_boost=1.4)
+    pdf_viewer(rf"certificados/{certificado}", width=725, height=500, resolution_boost=1.4)
     st.divider()
 
-adicionar_certificado(r"certificados/Power BI Impressionador.pdf", "118h", "20/11/2024", "02/01/2025", 
+adicionar_certificado(r"Power BI Impressionador.pdf", "118h", "20/11/2024", "02/01/2025", 
                       """Desenvolvi habilidades para criar dashboards dinâmicos e 
                       visualizações impactantes, dominando o tratamento de dados no 
                       Power Query e fórmulas DAX, das mais básicas até as mais avançadas.""")
 
-adicionar_certificado(r"certificados/Amazon AWS Certified Cloud Practitioner CLF-C02.pdf", "16h", "08/11/2024", "16/11/2024",
+adicionar_certificado(r"Análise de Dados Impressionadora.pdf", "104h", "06/07/2024", "20/12/2024",
+                      """Um curso abrangente que me prepara para atuar como analista de dados, 
+                      abordando as principais ferramentas do mercado. Aprendi a organizar e tratar dados no Excel, 
+                      criar dashboards interativos no Power BI, realizar consultas avançadas em SQL, 
+                      automatizar análises com Python e aplicar inteligência artificial para obter insights. 
+                      Esse conhecimento integrado me permite transformar dados em decisões estratégicas com confiança.""")
+
+adicionar_certificado(r"Amazon AWS Certified Cloud Practitioner CLF-C02.pdf", "16h", "08/11/2024", "16/11/2024",
                       """Sou uma pessoa fascinada pela computação em nuvem e 
                       em como ela inova o jeito de operar das empresas. 
                       Aprendi como começar na AWS com este curso que me 
                       prepara para a prova de certificação "AWS Cloud Practitioner", 
                       que testa conhecimentos básicos sobre os serviços da AWS.""")
 
-adicionar_certificado(r"certificados/SQL Impressionador.pdf", "90h", "26/09/2024", "24/10/2024",
+adicionar_certificado(r"SQL Impressionador.pdf", "90h", "26/09/2024", "24/10/2024",
                       """Aprendi a usar SQL para explorar, manipular e 
                       consultar bancos de dados com eficiência. 
                       O curso abordou desde conceitos básicos, 
                       como seleções e filtros, até técnicas avançadas 
                       para transformar dados em insights valiosos, 
                       essenciais para análises e tomadas de decisão.""")
-adicionar_certificado(r"certificados/Inteligência Artificial Impressionador.pdf", "55h", "06/09/2024", "23/09/2024", 
+adicionar_certificado(r"Inteligência Artificial Impressionador.pdf", "55h", "06/09/2024", "23/09/2024", 
                       """Descobri como integrar ferramentas de inteligência artificial, 
                       como ChatGPT, Gemini e Copilot, no dia a dia profissional, 
                       aumentando a produtividade e aplicando a tecnologia em 
                       diferentes áreas.""")
 
-adicionar_certificado(r"certificados/Python Impressionador.pdf", "124h", "07/03/2024", "10/07/2024",
+adicionar_certificado(r"Python Impressionador.pdf", "124h", "07/03/2024", "10/07/2024",
                       """Dominei os fundamentos e conhecimentos avançados de Python, 
                       explorando estruturas de dados, bibliotecas populares, 
                       e desenvolvendo diversos projetos práticos que simulam cenários do mercado de trabalho.""")

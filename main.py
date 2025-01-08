23ffe91fba8a2bb2d7bbcfc95dc3d961a78117eb
Giovani Bruno <giovainic@gmail.com>
2025-01-08 19:43:22 -0300

    bug do st.set_page_config corrigido

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import streamlit as st
 
-pg = st.navigation([
-    st.Page("Sobre.py", title="👨‍💻 Sobre mim"),
-    st.Page("Certificados.py", title="📃 Certificados"), 
-    st.Page("Leituras.py", title="📚 Leituras"),
-    st.Page("Projetos.py", title="💻 Projetos"),
-    st.Page("teste.py")
-    ], position='hidden')
+def main():
+    pg = st.navigation([
+        st.Page("Sobre.py", title="👨‍💻 Sobre mim"),
+        st.Page("Certificados.py", title="📃 Certificados"), 
+        st.Page("Leituras.py", title="📚 Leituras"),
+        st.Page("Projetos.py", title="💻 Projetos"),
+        st.Page("teste.py")
+        ], position='hidden')
+    
+    pg.run()
 
 def barra_navegacao():
     st.sidebar.page_link("Sobre.py")
@@ -14,4 +17,5 @@ def barra_navegacao():
     st.sidebar.page_link("Leituras.py")
     st.sidebar.page_link("Projetos.py")
 
-pg.run()
+if __name__ == "__main__":
+    main()

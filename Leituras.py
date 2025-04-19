d1cadd7aeefc003a2b12e1ad76849aa6080c7d08
Giovani Bruno <giovainic@gmail.com>
2025-04-19 19:17:05 -0300

    pagina leituras mostra os mais recentes primeiro

--- Leituras.py
+++ Leituras.py
@@ -11,24 +11,7 @@ st.write("""Aqui compartilho os livros que me ajudaram a expandir meu conhecimen
 A leitura constante é uma das minhas formas favoritas de aprender e crescer profissionalmente.""")
 st.divider()
 
-adicionar_livro(livros['Storytelling'], '''
-                Aprendi que a verdadeira essência de uma boa visualização de dados
-                vai além de simplesmente apresentar números. 
-               É sobre contar uma história com os dados de forma que o público
-                consiga entender a mensagem de maneira clara e envolvente. 
-               A autora reforça que a escolha de gráficos, o design visual
-                e o contexto são fundamentais para transmitir a informação de forma eficaz.''', frase="Há uma história em seus dados, mas suas ferramentas não sabem qual é essa história.")
-
-adicionar_livro(livros['Python Dados'], """Escrito pelo criador da biblioteca pandas, aprendi a importância 
-                de dominar as ferramentas do Python para lidar com dados de forma eficiente. 
-                A obra me proporcionou uma compreensão profunda de como o Python pode ser 
-                usado para explorar, limpar, manipular e visualizar dados. Desde o início, 
-                aprendi a trabalhar com o Pandas, uma das bibliotecas mais poderosas para 
-                análise de dados, o que me ajudou a organizar dados em DataFrames e realizar 
-                operações complexas de forma simples e rápida. 
-                Além disso, pude entender como utilizar o NumPy para trabalhar com arrays e 
-                operações numéricas, e como essas ferramentas se complementam perfeitamente 
-                no processamento de dados.""", frase="Uma preparação de dados eficiente pode aumentar significativamente a produtividade, permitindo que você passe mais tempo analisando dados e menos tempo preparando-o para análise.")
+adicionar_livro(livros["Hands on ML"], "Lendo! Feedback em breve...", frase="Aprendizado de máquina é a ciência (e a arte) da programação de computadores de modo que eles possam aprender com os dados.")
 
 adicionar_livro(livros['Data Science'], """O título do livro me enganou um pouco. Pelo nome, imaginei que 
                 fosse um material introdutório para quem quer começar na área de Data Science. No entanto, 
@@ -46,4 +29,21 @@ adicionar_livro(livros['Data Science'], """O título do livro me enganou um pouc
                 deseja aprofundar seus conhecimentos.
                 """, frase="Vivemos em mundo cada vez mais imerso em dados.")
 
-adicionar_livro(livros["Hands on ML"], "Lendo! Feedback em breve...", frase="Aprendizado de máquina é a ciência (e a arte) da programação de computadores de modo que eles possam aprender com os dados.")
+adicionar_livro(livros['Python Dados'], """Escrito pelo criador da biblioteca pandas, aprendi a importância 
+                de dominar as ferramentas do Python para lidar com dados de forma eficiente. 
+                A obra me proporcionou uma compreensão profunda de como o Python pode ser 
+                usado para explorar, limpar, manipular e visualizar dados. Desde o início, 
+                aprendi a trabalhar com o Pandas, uma das bibliotecas mais poderosas para 
+                análise de dados, o que me ajudou a organizar dados em DataFrames e realizar 
+                operações complexas de forma simples e rápida. 
+                Além disso, pude entender como utilizar o NumPy para trabalhar com arrays e 
+                operações numéricas, e como essas ferramentas se complementam perfeitamente 
+                no processamento de dados.""", frase="Uma preparação de dados eficiente pode aumentar significativamente a produtividade, permitindo que você passe mais tempo analisando dados e menos tempo preparando-o para análise.")
+
+adicionar_livro(livros['Storytelling'], '''
+                Aprendi que a verdadeira essência de uma boa visualização de dados
+                vai além de simplesmente apresentar números. 
+               É sobre contar uma história com os dados de forma que o público
+                consiga entender a mensagem de maneira clara e envolvente. 
+               A autora reforça que a escolha de gráficos, o design visual
+                e o contexto são fundamentais para transmitir a informação de forma eficaz.''', frase="Há uma história em seus dados, mas suas ferramentas não sabem qual é essa história.")
